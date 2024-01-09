481052695081fcb752901f3c6312dfac034531b1
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2024-01-09 21:13:58 +0900

    npm install styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
+* {
+  box-sizing: border-box;
+}
+body {
+  background-color: black;
+  color:white;
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
 `;
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <GlobalStyles />
+    </>
+  );
 }
 
 export default App;

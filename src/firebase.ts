b0403399996fe0c8ddb1e174b3640a006b2aa9b0
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2024-01-11 15:11:02 +0900

    auth

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,9 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDXyhnBn_c1sr_R9O3XM9tyyFawlfgcmIQ",
   authDomain: "pori-sns.firebaseapp.com",
@@ -13,5 +10,5 @@ const firebaseConfig = {
   appId: "1:180277115171:web:17d0bbd5fc982b12c7f496",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);

b23454c66e92310a36b10bc8c9259c079369368f
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2024-01-11 17:22:36 +0900

    Create Account

--- src/routes/create-account.tsx
+++ src/routes/create-account.tsx
@@ -1,5 +1,8 @@
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { useState } from "react";
 import { styled } from "styled-components";
+import { auth } from "../firebase";
+import { useNavigate } from "react-router-dom";
 
 const Wrapper = styled.div`
   height: 100%;
@@ -9,11 +12,9 @@ const Wrapper = styled.div`
   width: 420px;
   padding: 50px 0px;
 `;
-
 const Title = styled.h1`
   font-size: 42px;
 `;
-
 const Form = styled.form`
   margin-top: 50px;
   display: flex;
@@ -21,7 +22,6 @@ const Form = styled.form`
   gap: 10px;
   width: 100%;
 `;
-
 const Input = styled.input`
   padding: 10px 20px;
   border-radius: 50px;
@@ -35,13 +35,13 @@ const Input = styled.input`
     }
   }
 `;
-
 const Error = styled.span`
   font-weight: 600;
   color: tomato;
 `;
 
 export default function CreateAccount() {
+  const navigate = useNavigate();
   const [isLoading, setLoading] = useState(false);
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
@@ -59,12 +59,21 @@ export default function CreateAccount() {
       setPassword(value);
     }
   };
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    if (isLoading || name === "" || email === "" || password === "") return;
     try {
-      // create an account
-      // set the name of the user.
-      // redirect to the home page
+      setLoading(true);
+      const credentials = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      console.log(credentials.user);
+      await updateProfile(credentials.user, {
+        displayName: name,
+      });
+      navigate("/");
     } catch (e) {
       // setError
     } finally {
@@ -73,7 +82,7 @@ export default function CreateAccount() {
   };
   return (
     <Wrapper>
-      <Title>Log into 𝕏</Title>
+      <Title>Join 𝕏</Title>
       <Form onSubmit={onSubmit}>
         <Input
           onChange={onChange}

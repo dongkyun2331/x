533adbcc362a708130cf41e8003846be336ed49c
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2024-01-11 15:18:29 +0900

    create account ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle, styled } from "styled-components";
 import reset from "styled-reset";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Layout from "./components/layout";
@@ -47,6 +47,12 @@ const GlobalStyles = createGlobalStyle`
   }
 `;
 
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+`;
+
 function App() {
   const [isLoading, setLoading] = useState(true);
   const init = async () => {
@@ -57,10 +63,10 @@ function App() {
     init();
   }, []);
   return (
-    <>
+    <Wrapper>
       <GlobalStyles />
       {isLoading ? <LoadingScreen /> : <RouterProvider router={router} />}
-    </>
+    </Wrapper>
   );
 }
 

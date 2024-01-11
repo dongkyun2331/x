910840afc8ae3e3db23ff80f9938b2c35883afe1
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2024-01-11 15:05:58 +0900

    firebase

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDXyhnBn_c1sr_R9O3XM9tyyFawlfgcmIQ",
+  authDomain: "pori-sns.firebaseapp.com",
+  projectId: "pori-sns",
+  storageBucket: "pori-sns.appspot.com",
+  messagingSenderId: "180277115171",
+  appId: "1:180277115171:web:17d0bbd5fc982b12c7f496",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
